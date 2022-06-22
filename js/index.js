cb9fd1444a06189444fa712669b0e87c44ab4e23
chibuzordaniel <starchrischibuzor@gmail.com>
2022-06-22 02:21:20 +0100

    update file

--- js/index.js
+++ js/index.js
@@ -7,4 +7,6 @@ document.write(Name2)
 const Name =
  '<h2> First Name: Christian <br> Middle Name: Chibuzor  <br> Last Name: Daniel <br>  Height:  5.2 feet  <br> Country: Nigeria</h2>';
 
+
+ 
 document.write(Name)
