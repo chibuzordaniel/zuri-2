dedffeb4db3240ba60a35daa496eb581ddffb0c6
chibuzordaniel <starchrischibuzor@gmail.com>
2022-06-22 02:18:36 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+const Name2 = 
+'<h1>Introduction to Javascript</h1>'
+
+document.write(Name2)
+
+const Name =
+ '<h2> First Name: Christian <br> Middle Name: Chibuzor  <br> Last Name: Daniel <br>  Height:  5.2 feet  <br> Country: Nigeria</h2>';
+
+document.write(Name)
